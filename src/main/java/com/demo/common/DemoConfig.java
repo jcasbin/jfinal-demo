1007f4fae85d6a5ae05c04bca2e49c72d320ab8c
Yang Luo <hsluoyz@qq.com>
2018-05-01 21:15:56 +0800

    Use the IDEA startup by default.

--- src/main/java/com/demo/common/DemoConfig.java
+++ src/main/java/com/demo/common/DemoConfig.java
@@ -34,12 +34,12 @@ public class DemoConfig extends JFinalConfig {
 		/**
 		 * 特别注意：Eclipse 之下建议的启动方式
 		 */
-		JFinal.start("src/main/webapp", 80, "/", 5);
+		// JFinal.start("src/main/webapp", 80, "/", 5);
 		
 		/**
 		 * 特别注意：IDEA 之下建议的启动方式，仅比 eclipse 之下少了最后一个参数
 		 */
-		// JFinal.start("src/main/webapp", 80, "/");
+		JFinal.start("src/main/webapp", 80, "/");
 	}
 	
 	/**
